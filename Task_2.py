dedf2bf6e47f5d639671fcd1964734df9eb10c3a
Tameka Roberson <ttrobersoncreate@gmail.com>
2024-03-28 22:04:53 -0400

    Task 2 Completed

--- Task_2.py
+++ Task_2.py
@@ -1,1 +1,8 @@
 # Quick Decisions: The Event Planner
+
+attendees = int(input("Enter the numbers of attendees: "))
+print("Large Hall" if attendees > 100 else "Conference Room" if attendees <= 75 and attendees >= 30 else "Audio room")
+
+
+foodRecommendation = input("Are you a vegan? ")
+print("Veggie Delight Caterers" if foodRecommendation == "yes" else "Gourmet Meals Caterers") # <--- was this suppose to be it's own line of code?
