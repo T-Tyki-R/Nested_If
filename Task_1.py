87dc7fa6e858a45f641cfec3d0b65ff515cbf98f
Tameka Roberson <ttrobersoncreate@gmail.com>
2024-03-28 21:36:58 -0400

    Task 1 Completed

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,14 @@
 # Nested Decisions: The Adventure Game 
+
+
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest":
+    action = input("climb a tree or cross a river? ")
+    print("You found a bird's nest" if action == "tree" else "You found a boat!")
+elif place == "cave":
+    action = input("light a torch or proceed in the dark? ")
+    print("SHHHH!!! You found a sleeping dragon!" if action == "torch" else "Oops... you bumped into an angry dragon!")
+else:
+    pass
+
